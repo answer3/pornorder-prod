21fdcf06133f5c49a620c2f0ac0e48adf6da99c2
alex <answer3ster@gmail.com>
2016-07-12 19:50:36 +0200

    Pagination

--- pornorder/resources/views/elements/pagination.blade.php
+++ pornorder/resources/views/elements/pagination.blade.php
@@ -1,8 +1,28 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination-block pagination">
+                    <li>
+                        <a class="prev" href="#">Prev</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">...</a>
+                    </li>
+                    <li>
+                        <a href="#">10</a>
+                    </li>
+                    <li>
+                        <a class="next" href="#">Next</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
--- pornorder/resources/views/order/trending.blade.php
+++ pornorder/resources/views/order/trending.blade.php
@@ -26,35 +26,7 @@
 			@endfor
         </div>
 		
-				        <!-- Pagination -->
-        <div class="row text-center">
-            <div class="col-lg-12">
-                <ul class="pagination-block pagination">
-                    <li>
-                        <a class="prev" href="#">Prev</a>
-                    </li>
-                    <li class="active">
-                        <a href="#">1</a>
-                    </li>
-                    <li>
-                        <a href="#">2</a>
-                    </li>
-                    <li>
-                        <a href="#">3</a>
-                    </li>
-                    <li>
-                        <a href="#">...</a>
-                    </li>
-                    <li>
-                        <a href="#">10</a>
-                    </li>
-                    <li>
-                        <a class="next" href="#">Next</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <!-- /.row -->
+		@include('elements.pagination')
 
         @include('elements.section_header',['title'=>'Most popular orders'])
 
